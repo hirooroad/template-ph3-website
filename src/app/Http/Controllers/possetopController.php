1113d97b80e3782393a88db484c60d3772c5579a
hiroo-road <hirofumi7678@gmail.com>
2024-05-29 22:25:21 +0900

    エラー修正5/29

---
+++ src/app/Http/Controllers/possetopController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class possetopController extends Controller
+{
+    public function possetop() 
+    {
+        return view('possetop');
+    }
+
+}
---
+++ src/database/migrations/2024_05_28_141311_create_quizees_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+        Schema::dropIfExists('quizees');
+        Schema::dropIfExists('quiz_questions');
+        Schema::dropIfExists('questions');
+        Schema::dropIfExists('choices');
+    }
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizeees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('カテゴライズしたクイズ名 ex.) ITクイズ');
+            $table->timestamps();
+        });
+
+        Schema::create('quiz_questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('image')->comment('設問画像 ex.) /image/sample.jpg');
+            $table->string('text')->comment('設問 ex.) 日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？');
+            $table->string('supplement')->nullable()->comment('設問補足');
+            $table->unsignedBigInteger('quiz_id');
+            $table->timestamps();
+
+            $table->foreign('quiz_id')->references('id')->on('quizeees');
+        });
+
+        Schema::create('choices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('question_id');
+            $table->string('text')->comment('選択肢 ex.) 約79万人');
+            $table->boolean('is_correct');
+            $table->timestamps();
+
+            $table->foreign('question_id')->references('id')->on('quiz_questions');
+        });
+    }
+
+};
+
